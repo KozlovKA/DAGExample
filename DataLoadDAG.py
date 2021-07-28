446eff42dc63385e76b2f80947761df86ff8f5ff
Kazlou_KA <kirillkozlou@yandex.ru>
2021-07-28 12:26:36 +0300

    Fixing path in the dag

--- DataLoadDAG.py
+++ DataLoadDAG.py
@@ -28,7 +28,7 @@ spark_config = {
 
 t3 = SparkSubmitOperator(task_id='DataLoad',
                          name="DataLoad",
-                         application="local:///home/ko3lof/testing-assembly-0.1.jar",
+                         application="local:///jar/testing-assembly-0.1.jar",
                          dag=dag,
                          conn_id="spark",
                          verbose=1,
