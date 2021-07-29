a2e12f0d34fd7295ca385437f080d3bad623b457
Kazlou_KA <kirillkozlou@yandex.ru>
2021-07-29 18:50:42 +0300

    Fixing permisson of Dockerfile

--- DataLoadDAG.py
+++ DataLoadDAG.py
@@ -26,7 +26,7 @@ dag = DAG("DataLoad",
 
 t2 = SparkSubmitOperator(task_id='DataLoad',
                          name='DataLoad',
-                         application='local:///app/ibaTask-assembly-0.1.jar',
+                         application='local:///jar/ibaTask-assembly-0.1.jar',
                          dag=dag,
                          conf={
                              "spark.hadoop.fs.stocator.scheme.list": "cos",
